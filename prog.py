1e0f0e8839f0a9c72a8ce853d785df9f27919686
kiran23232 <54184915+kiran23232@users.noreply.github.com>
2019-08-16 23:19:11 +0530

    comment

--- prog.py
+++ prog.py
@@ -1,6 +1,6 @@
-@ 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-@ What is the sum of the digits of the number 21000?
+# What is the sum of the digits of the number 21000?
 
 
 number = str(2**1000)
